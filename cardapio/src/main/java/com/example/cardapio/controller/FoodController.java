9a776f79bdd3cad8d06822938ad64a3c5cc39040
Douglas Alves <douglasmonttozo@hotmail.com.br>
2023-12-26 12:59:25 -0300

    req/res aplicattion

--- cardapio/src/main/java/com/example/cardapio/controller/FoodController.java
+++ cardapio/src/main/java/com/example/cardapio/controller/FoodController.java
@@ -1,17 +1,28 @@
 package com.example.cardapio.controller;
 
-import com.example.cardapio.food.Food;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.cardapio.food.FoodRepository;
+import com.example.cardapio.food.FoodRequestDTO;
+import com.example.cardapio.food.FoodResponseDTO;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
 //RestController indica que isso é um controller da nossa aplicação
 //mas tem que informar o endpoint que controla
 @RestController
 @RequestMapping("food")
 public class FoodController {
+    @Autowired
+    private FoodRepository repository;
     //GetMapping = classe é chamada quando o endpoint é chamado
+    @PostMapping
+    public void saveFood(@RequestBody FoodRequestDTO data){
+        repository.save(data);
+    }
     @GetMapping
-    public void getAll(){
-        Food food
+    public List<FoodResponseDTO> getAll(){
+        List<FoodResponseDTO> foodList=repository.findAll().stream().map(FoodResponseDTO::new).toList();
+        return foodList;
     }
 }
--- cardapio/src/main/java/com/example/cardapio/food/Food.java
+++ cardapio/src/main/java/com/example/cardapio/food/Food.java
@@ -1,10 +1,18 @@
 package com.example.cardapio.food;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 //@Table indica que é uma tabela/entidade do banco de dados
 @Table(name="foods")
 @Entity(name="foods")
+@Getter //gera todos os metodos de get da classe toda
+@NoArgsConstructor // declara um constructor que nao recebe nenhum argumento
+@AllArgsConstructor // declara um constructor que recebe todos os argumentos
+@EqualsAndHashCode(of="id") //id como representacao
 public class Food {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
